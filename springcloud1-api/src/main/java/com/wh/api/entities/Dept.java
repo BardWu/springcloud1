dedebccc3083252ecf2583bec2252ac8ea193c3d
wu heng <1034250349@qq.com>
2020-08-21 17:34:54 +0800

    初始化一个完整的节点

--- springcloud1-api/src/main/java/com/wh/api/entities/Dept.java
+++ springcloud1-api/src/main/java/com/wh/api/entities/Dept.java
@@ -1,10 +1,16 @@
 package com.wh.api.entities;
 
+import lombok.Data;
+
 /**
  * @program: springcloud1
  * @description:
  * @author: wh
  * @create: 2020-08-20 19:39
  **/
+@Data
 public class Dept {
+
+    private int id;
+    private String deptName;
 }
