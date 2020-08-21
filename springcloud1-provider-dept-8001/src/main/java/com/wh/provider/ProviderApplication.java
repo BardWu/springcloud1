ab4129b770aa18fe3985399ef56aebdda36b0bd6
wu heng <1034250349@qq.com>
2020-08-21 17:35:41 +0800

    初始化一个节点

---
+++ springcloud1-provider-dept-8001/src/main/java/com/wh/provider/ProviderApplication.java
@@ -0,0 +1,17 @@
+package com.wh.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: springcloud1
+ * @description:
+ * @author: wh
+ * @create: 2020-08-21 17:12
+ **/
+@SpringBootApplication
+public class ProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class,args);
+    }
+}
---
+++ springcloud1-provider-dept-8001/src/main/java/com/wh/provider/dao/DeptDao.java
@@ -0,0 +1,22 @@
+package com.wh.provider.dao;
+
+import com.wh.api.entities.Dept;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @program: springcloud1
+ * @description:
+ * @author: wh
+ * @create: 2020-08-21 16:43
+ **/
+@Mapper
+public interface DeptDao {
+
+    boolean addDept(Dept dept);
+
+    List<Dept> findAll();
+
+
+}
